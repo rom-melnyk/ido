a2d04a2604e3c4a4110c506e104a53f01497a9d8
Roman Melnyk <email.rom.melnyk@gmail.com>
2019-05-21 21:31:47 +0200

    Migrate Entity to ActiveRecords

--- server/db/entity/challenge.ts
+++ server/db/entity/challenge.ts
@@ -1,7 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Challenge {
+export class Challenge extends BaseEntity {
 
   @PrimaryGeneratedColumn()
   id!: number;
