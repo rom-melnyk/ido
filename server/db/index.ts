194120bb4718351a36d47faf866fe1232692dfd8
Roman Melnyk <email.rom.melnyk@gmail.com>
2019-05-21 22:31:39 +0200

    Introduce Express and separate DB config from server one

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,5 +1,5 @@
 import { createConnection } from 'typeorm';
-import dbConfig from './config';
+import dbConfig from './db-config';
 
 export async function connect() {
   return await createConnection(dbConfig);
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,19 @@
-import 'reflect-metadata';
+import 'reflect-metadata'; // required for TypeORM
+import express from 'express';
 import { connect } from './db';
+import * as serverConfig from './server-config';
 
 (async () => {
-  try {
-    const connection = await connect();
-    console.info(connection && 'Me works');
-  } catch (e) {
-    console.error('Cannot establish DB connection', e);
+  const app = express();
+
+  const connection = await connect();
+  if (connection) {
+    console.info('DB connection established');
+  } else {
+    throw new Error('Cannot establish DB connection');
   }
+
+  app.listen(serverConfig.port, () => {
+    console.info(`The iDo server is listening at :${serverConfig.port}`);
+  })
 })();
