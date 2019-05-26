0ad2bc8197bb27b7c85f56d03942fed3cf5c4830
Roman Melnyk <email.rom.melnyk@gmail.com>
2019-05-26 15:33:57 +0200

    Update Challenge table migration

--- server/db/migration/1558467942586-create-challenge-table.ts
+++ server/db/migration/1558467942586-create-challenge-table.ts
@@ -1,13 +1,36 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class createChallengeTable1558467942586 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query("CREATE TABLE `challenge` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `created_at` datetime NOT NULL, `created_by` int NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
-    }
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(new Table({
+      name: 'challenge',
+      columns: [{
+        name: 'id',
+        type: 'int',
+        isPrimary: true,
+      }, {
+        name: 'name',
+        type: 'varchar',
+        isNullable: false,
+      }, {
+        name: 'created_at',
+        type: 'datetime',
+        isNullable: false,
+      }, {
+        name: 'created_by',
+        type: 'int',
+        isNullable: false,
+      }, {
+        name: 'is_active',
+        type: 'boolean',
+        default: true,
+      }]
+    }));
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query("DROP TABLE `challenge`");
-    }
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('challenge');
+  }
 
 }
